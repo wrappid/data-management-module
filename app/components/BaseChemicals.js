fb51f2d083e9064e2ac13ff17fbc897b8d51242c
Ananta Kumar Ghosh <antaghosh@gmail.com>
2024-01-25 22:37:29 +0530

    ci(config): :wrench: test case
    dummy commit
    
    Ref: #10, wrappid/wrappid-module#19

--- app/components/BaseChemicals.js
+++ app/components/BaseChemicals.js
@@ -1,5 +1,7 @@
 import { CoreTypographyBody1 } from "@wrappid/core";
 
+let test;
+
 export default function BaseChemicals() {
   return (<CoreTypographyBody1>Base Chemicals</CoreTypographyBody1>);
 }
