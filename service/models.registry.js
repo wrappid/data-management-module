4c3c9418fb391f7b61cec06b909291f8f64e724e
PritamIT2023 <pritammondal8981@email.com>
2024-01-25 17:50:59 +0530

    feat: model scatter
    change model as per wrappid requirments
    
    Ref: #9

--- service/models.registry.js
+++ service/models.registry.js
@@ -1,71 +1,10 @@
-const Complaints = require("./models/Complaints.model");
-const MedicineCompanies = require("./models/MedicineCompany.model");
-const MedicineDetails = require("./models/MedicineDetails.model");
-const MedicinePackages = require("./models/MedicinePackages.model");
-const Medicines = require("./models/Medicines.model");
-const ChemicalCompositions = require("./models/ChemicalCompositions.model");
-const CompositionDepartments = require("./models/CompositionDepartments.model");
-const Formulations = require("./models/Formulations.model");
-const Departments = require("./models/Departments.model");
-const Diagnoses = require("./models/Diagnosis.model");
-const Procedures = require("./models/Procedures.model");
-const Tests = require("./models/Tests.model");
 const Applications = require("./models/Applications.model");
 const DataTableOptions = require("./models/DataTableOptions.model");
 const MasterData = require("./models/MasterData.model");
-const Reffers = require("./models/Reffers.model");
 const SupportedLanguages = require("./models/SupportedLanguages.model");
+const StringValues = require("./models/StringValues.model");
 const modelsRegistry = {
-    
-    
-"Complaints":{
-    database: "application",
-    model: Complaints
-},
-"MedicineCompanies":{
-    database: "application",
-    model: MedicineCompanies
-},
-"MedicineDetails":{
-    database: "application",
-    model: MedicineDetails
-},
-"MedicinePackages":{
-    database: "application",
-    model: MedicinePackages
-},
-"Medicines":{
-    database: "application",
-    model: Medicines
-},
-"ChemicalCompositions":{
-    database: "application",
-    model: ChemicalCompositions
-},
-"CompositionDepartments":{
-    database: "application",
-    model: CompositionDepartments
-},
-"Formulations":{
-    database: "application",
-    model: Formulations
-},
-"Departments":{
-    database: "application",
-    model: Departments
-},
-"Diagnoses":{
-    database: "application",
-    model: Diagnoses
-},
-"Procedures":{
-    database: "application",
-    model: Procedures
-},
-"Tests":{
-    database: "application",
-    model: Tests
-},
+ 
 "Applications":{
     database: "application",
     model: Applications
@@ -78,14 +17,14 @@ const modelsRegistry = {
     database: "application",
     model: MasterData
 },
-"Reffers":{
-    database: "application",
-    model: Reffers
-},
 "SupportedLanguages":{
     database: "application",
     model: SupportedLanguages
 },
+"StringValues": {
+    database: "application",
+    model: StringValues
+}
 };
 
 exports.modelsRegistry = modelsRegistry;
